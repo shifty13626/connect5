8e7dee2affdf1485e973e5655d76dac5a7366473
hugoTAMBERI <tamberihugo@gmail.com>
2021-05-05 14:19:51 +0200

    feature get_next_move / alpha_beta_pruning

--- state.py
+++ state.py
@@ -1,6 +1,9 @@
 BOARD_WIDTH = 12
 BOARD_HEIGHT = 8
 
+infinity = float('inf')
+MAX_DEPTH = 10
+
 
 class State:
     def __init__(self, ai_position, game_position):
@@ -13,52 +16,30 @@ class State:
 
     @staticmethod
     def is_winning_state(position):
-        if position & (position >> (BOARD_HEIGHT - 1)) & (position >> ((BOARD_HEIGHT - 1) * 2)) & ((BOARD_HEIGHT - 1) * 3) & (position >> ((BOARD_HEIGHT - 1) * 4)) != 0:
+        if position & (position >> (BOARD_HEIGHT - 1)) & (position >> ((BOARD_HEIGHT - 1) * 2)) & (
+                (BOARD_HEIGHT - 1) * 3) & (position >> ((BOARD_HEIGHT - 1) * 4)) != 0:
             return True  # diagonal \
-        if position & (position >> (BOARD_HEIGHT + 1)) & (position >> ((BOARD_HEIGHT + 1) * 2)) & (position >> ((BOARD_HEIGHT + 1) * 3)) & (position >> ((BOARD_HEIGHT + 1) * 4)) != 0:
+        if position & (position >> (BOARD_HEIGHT + 1)) & (position >> ((BOARD_HEIGHT + 1) * 2)) & (
+                position >> ((BOARD_HEIGHT + 1) * 3)) & (position >> ((BOARD_HEIGHT + 1) * 4)) != 0:
             return True  # diagonal /
-        if position & (position >> BOARD_HEIGHT) & (position >> (BOARD_HEIGHT * 2)) & (position >> (BOARD_HEIGHT * 3)) & (position >> (BOARD_HEIGHT * 4)) != 0:
+        if position & (position >> BOARD_HEIGHT) & (position >> (BOARD_HEIGHT * 2)) & (
+                position >> (BOARD_HEIGHT * 3)) & (position >> (BOARD_HEIGHT * 4)) != 0:
             return True  # horizontal
         if position & (position >> 1) & (position >> 2) & (position >> 3) & (position >> 4) != 0:
             return True  # vertical
         return False
 
-
     def is_terminal_state(self):
-        #TODO
+        # TODO
         return True
 
     def get_heuristic(self):
         # TODO
         return 42
+
     def get_children(self):
         # TODO
         return self
-
-    # def alpha_beta_pruning(self, alpha, beta, depth, max_depth):
-    #     if self.is_terminal_state() or depth > max_depth:
-    #         return self.get_heuristic()
-    #     if depth % 2 == 0:
-    #         for child in self.get_children():
-
-
-       # else:
-
-# evaluate (node, alpha, beta)
-#      if node is a leaf
-#         return the utility value of node
-#      if node is a minimizing node
-#         for each child of node
-#             beta = min (beta, evaluate (child, alpha, beta))
-#             if beta <= alpha
-#                 return beta
-#             return beta
-#      if node is a maximizing node
-#         for each child of node
-#             alpha = max (alpha, evaluate (child, alpha, beta))
-#             if beta <= alpha
-#                 return alpha
-#             return alpha
 
     def print_board(self):
         ai_board, total_board = self.ai_position, self.game_position
@@ -72,3 +53,40 @@ class State:
                 else:
                     print("0", end='')
         print("")
+
+    def get_next_move(self):
+
+        def alpha_beta_pruning(state, alpha, beta, depth, max_depth):
+            if state.is_terminal_state() or depth > max_depth:
+                return state.get_heuristic()
+            if depth % 2 == 0:
+                value = infinity
+                for child in state.get_children():
+                    if child in known_states:
+                        continue
+                    value = min(value, alpha_beta_pruning(child, alpha, beta, depth + 1, max_depth))
+                    known_states[child] = value
+                    if alpha >= value:
+                        return value
+                    beta = min(beta, value)
+            else:
+                value = -infinity
+                for child in state.get_children():
+                    if child in known_states:
+                        continue
+                    value = max(value, alpha_beta_pruning(child, alpha, beta, depth + 1, max_depth))
+                    known_states[child] = alpha
+                    if value >= beta:
+                        return value
+                    alpha = min(alpha, value)
+            return value
+
+        known_states = {}
+        best_state = None
+        best_score = -infinity
+        for state in self.get_children():
+            v = alpha_beta_pruning(state, -infinity, infinity, 1, MAX_DEPTH)
+            if v > best_score:
+                best_score = v
+                best_state = state
+        return best_state
